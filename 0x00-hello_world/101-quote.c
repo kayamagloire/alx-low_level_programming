d6f2d23c4d0ab238d4d670bb29d1d8ee36f49c1d
kayamagloire <kayamagloire2@gmail.com>
2023-06-18 18:45:07 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This c program prints without using the functions put and printf
+ * Return 1 (success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
