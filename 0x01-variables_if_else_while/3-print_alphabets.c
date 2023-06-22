fdcf5a107163820d64795eed68a766ed886a1e62
kayamagloire <kayamagloire2@gmail.com>
2023-06-21 17:50:57 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - _print alphabets is a program that prints the alphabet
+ * in lowercase, and then in uppercase, followed by a new line.
+ * Return: 0 (succes)
+ */
+
+int main(void)
+{
+        char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alpha[i]);
+                putchar('\n');
+        }
+        return (0);
+}
