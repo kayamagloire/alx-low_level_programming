f7c632966bf8ed33f9f6ec42a679a66a6aa134e4
kayamagloire <kayamagloire2@gmail.com>
2023-06-23 15:24:53 -0700

    comment on infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-        while (i < 10) /* This is the part of the loop that has a bug */
-        {
-                putchar(i);
-        }
+	while (i < 10) /* This is the part of the loop that has a bug */
+	{
+		putchar(i);
+	}
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
