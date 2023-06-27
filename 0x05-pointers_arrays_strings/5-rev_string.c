f598fff6945046324ececc27e7e681e7cb8366fd
kayamagloire <kayamagloire2@gmail.com>
2023-06-26 19:49:12 -0700

    puts

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,13 @@ void rev_string(char *s)
 	int counter = 0;
 	int i;
 
-	while (s[counter]!= '\0')
+	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
-		rev = s[i];
+		re = s[i];
 		s[i] = s[counter];
-		s[counter] = rev;
+		s[counter] = re;
 	}
 }
